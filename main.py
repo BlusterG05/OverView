4660f83dce97d9c40eb7391957de24a749a40a30
Jeremy <jerycohe05@gmail.com>
2024-01-31 10:48:27 -0500

    Main de overview

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from camera_output import CameraOutput
 from interface import Interface
 from transcription_output import TranscriptionOutput
 from transcription_history import TranscriptionHistory
-# Ventana Principal
+
 root = tk.Tk()
 root.title("Software de Lenguaje de Señas")
 # Interfaz
@@ -12,8 +12,9 @@ interface = Interface(root)
 model_path = 'overviewmodel/modelos/overview_model.h5'
 # Instancias de TranscriptionOutput y TranscriptionHistory
 transcription_output = TranscriptionOutput(interface.transcription_frame)
-# Creating the transcription history
+
 transcription_history = TranscriptionHistory(interface.history_frame)
+
 # Widget de salida para mostrar las predicciones
 output_widget = interface.transcription_output_label  
 app = CameraOutput(interface.camera_frame, model_path, output_widget, transcription_output, transcription_history)
